f55785cef654f988b4106ee71777952bf8267fcc
Hayrullah DEDELER <h.dede515151@gmail.com>
2022-08-16 14:08:30 +0300

    ilkdegisiklik

--- src/KisiselKullanim/GithubKullanimi1.java
+++ src/KisiselKullanim/GithubKullanimi1.java
@@ -3,5 +3,7 @@ package KisiselKullanim;
 public class GithubKullanimi1 {
     public static void main(String[] args) {
         System.out.println("Hello Github");
+
+        System.out.println(" Thats's it");
     }
 }
