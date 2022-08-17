04d7d143e5200975bc0b966d94d970eb123cbe72
Hayrullah DEDELER <h.dede515151@gmail.com>
2022-08-17 12:06:39 +0300

    Hayrullah branch 2. degisiklik

--- src/KisiselKullanim/GithubKullanimi1.java
+++ src/KisiselKullanim/GithubKullanimi1.java
@@ -5,5 +5,7 @@ public class GithubKullanimi1 {
         System.out.println("Hello Github");
 
         System.out.println(" Thats's it");
+
+        System.out.println("hayrullah branch inde orak class da degisiklik yaptim");
     }
 }
